15eef3d5739f1297ff621fe3c53cb423cecea2d8
Maira Naeem <mairanoor412@gmail.com>
2025-07-20 21:21:02 +0500

    add a console

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -102,7 +102,7 @@ const Header = () => {
             </div>
 
             {/* Mobile Sidebar */}
-            <ResponsiveMenu open={open} />
+            <ResponsiveMenu open={open}  setopen={setopen} />
 
         </div>
     )
--- src/app/components/responsive-menu/responsive-menu.tsx
+++ src/app/components/responsive-menu/responsive-menu.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 
-const ResponsiveMenu = ({ open }: any) => {
+const ResponsiveMenu = ({ open, setopen}: any) => {
     return (
         <AnimatePresence mode="wait">
 
@@ -16,16 +16,16 @@ const ResponsiveMenu = ({ open }: any) => {
                     <div className="bg-[#B88E2F] opacity-80 py-10 m-6 rounded-3xl md:hidden">
                         <ul className="flex flex-col justify-center items-center gap-10 ">
                             <li className="text-[16px] font-[500] text-primary">
-                                <Link href="/"  className="hover:underline hover:text-black"> Home </Link>
+                                <Link href="/" onClick={() => setopen(false)} className="hover:underline hover:text-black"> Home </Link>
                             </li>
                             <li className="text-[16px] font-[500] text-primary">
-                                <Link href="/shop"  className="hover:underline hover:text-black">  Shop </Link>
+                                <Link href="/shop" onClick={() => setopen(false)} className="hover:underline hover:text-black">  Shop </Link>
                             </li>
                             <li className="text-[16px] font-[500] text-primary">
-                                <Link href="/blog"  className="hover:underline hover:text-black"> Blog </Link>
+                                <Link href="/blog" onClick={() => setopen(false)}  className="hover:underline hover:text-black"> Blog </Link>
                             </li>
                             <li className="text-[16px] font-[500] text-primary">
-                                <Link href="/contact"  className="hover:underline hover:text-black"> Contact </Link>
+                                <Link href="/contact" onClick={() => setopen(false)} className="hover:underline hover:text-black"> Contact </Link>
                             </li>
 
                         </ul>
